f123f9c8d501cdb1eac569301813eef925072d32
Kyryl Krylov, CPA <k.krylov@creatio.com>
2021-02-17 19:32:16 +0200

    End of day 2

---
+++ GuidedLearning_Feb/Files/EventListeners/ContactEventListener.cs
@@ -0,0 +1,169 @@
+﻿using Terrasoft.Core;
+using Terrasoft.Core.Entities;
+using Terrasoft.Core.Entities.Events;
+
+namespace GuidedLearning_Feb.Files.EventListeners
+{
+	/// <summary>
+	/// Listener for 'EntityName' entity events.
+	/// </summary>
+	/// <seealso cref="Terrasoft.Core.Entities.Events.BaseEntityEventListener" />
+	[EntityEventListener(SchemaName = "Contact")]
+	class ContactEventListener : BaseEntityEventListener
+	{
+		#region Enum
+		#endregion
+
+		#region Delegates
+		#endregion
+
+		#region Constants
+		#endregion
+
+		#region Fields
+
+		#region Fileds : Private
+		#endregion
+
+		#region Fileds : Protected
+		#endregion
+
+		#region Fileds : Internal
+		#endregion
+
+		#region Fileds : Protected Internal
+		#endregion
+
+		#region Fileds : Public
+		#endregion
+
+		#endregion
+
+		#region Properties
+
+		#region Properties : Private
+		#endregion
+
+		#region Properties : Protected
+		#endregion
+
+		#region Properties : Internal
+		#endregion
+
+		#region Properties : Protected Internal
+		#endregion
+
+		#region Properties : Public
+		#endregion
+
+		#endregion
+
+		#region Events
+		#endregion
+
+		#region Methods
+
+		#region Methods : Private
+
+		#endregion
+
+		#region Methods : Public
+
+		#region Methods : Public : OnSave
+		public override void OnSaving(object sender, EntityBeforeEventArgs e)
+		{
+			base.OnSaving(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+
+
+
+
+			string newName = entity.GetTypedColumnValue<string>("Name");
+			string oldName = entity.GetTypedOldColumnValue<string>("Name");
+			
+
+			EntityValidationMessage evm = new EntityValidationMessage
+			{
+				Column = entity.Schema.Columns.FindByName("Name"),
+				MassageType = Terrasoft.Common.MessageType.Error,
+				Text = "Error validation from clio"
+			};
+
+
+
+
+
+			entity.ValidationMessages.Add(evm);
+
+			if(newName.Contains("Andrew "))
+			{
+				e.IsCanceled = true;
+			}
+
+			
+			//entity.SetColumnValue("Notes", $"Old:{oldName} - New:{newName}");
+
+		}
+		public override void OnSaved(object sender, EntityAfterEventArgs e)
+		{
+			base.OnSaved(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+
+			
+
+		}
+		#endregion
+
+		#region Methods : Public : OnInsert
+		public override void OnInserting(object sender, EntityBeforeEventArgs e)
+		{
+			base.OnInserting(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+		}
+		public override void OnInserted(object sender, EntityAfterEventArgs e)
+		{
+			base.OnInserted(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+		}
+		#endregion
+
+		#region Methods : Public : OnUpdate
+		public override void OnUpdating(object sender, EntityBeforeEventArgs e)
+		{
+			base.OnUpdating(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+		}
+		public override void OnUpdated(object sender, EntityAfterEventArgs e)
+		{
+			base.OnUpdated(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+		}
+		#endregion
+
+		#region Methods : Public : OnDelete
+		public override void OnDeleting(object sender, EntityBeforeEventArgs e)
+		{
+			base.OnDeleting(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+			e.IsCanceled = true;
+		}
+		public override void OnDeleted(object sender, EntityAfterEventArgs e)
+		{
+			base.OnDeleted(sender, e);
+			Entity entity = (Entity)sender;
+			UserConnection userConnection = entity.UserConnection;
+		}
+		#endregion
+
+		#endregion
+
+		#endregion
+	}
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Development on Creatio Platform (621608)
+
+
+## Session Recordings
+|Day | Video Link |
+|:--:|:----------: |
+|1|[Video][d1v]||
+|2|[Video][d2v]||
+<!--
+|3|[Video][d3v]|[Chat][d3c]|
+|4|[Video][d4v]||
+|5|[Video][d5v]|[Chat][d5c]|
+|6|[Video][d6v]||
+|7|[Video][d7v]|[Chat][d7c]|
+|8|[Video][d8v]||
+-->
+
+
+### How to articles
+- [Enable development in FileSystem Mode][fsm]
+- [Custom Logging][logger]
+- [Clio][clio]
+
+<!-- ## Feedback
+Thank you for having taken development training with me. In order to make this class better, please offer your [feedback][] through feedback form. It is very important for me to know where and how I can improve. -->
+
+
+<!-- Named Links--->
+[d1v]: https://creatio-global.zoom.us/rec/share/3TVEa7VjKqz2ZqC-ebUSx3vaDNPwhVN0Go7-_mrnM3bkLOGK8SZYrOMFuw-4dj62.k8AMbU9Q63MjSrKm?startTime=1610982009000
+[d2v]: https://
+
+
+[feedback]:https://forms.office.com/Pages/ResponsePage.aspx?id=-6Jce0OmhUOLOTaTQnDHFs1n4KjdfnVBtjvFqBN3Vk9UNjRZQVFRQ0E1UDBLMERaMlZUMFVYNzlKTy4u
+
+
+
+[fsm]: https://github.com/Academy-Creatio/TrainingProgramm/wiki/Enable-development-in-FileSystem-Mode
+[logger]:https://github.com/Academy-Creatio/TrainingProgramm/wiki/Custom-Logging-with-NLog
+[clio]:https://github.com/Advance-Technologies-Foundation/clio/blob/master/README.md
